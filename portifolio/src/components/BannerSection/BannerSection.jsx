b77799f05dcb5736350b143674f0a15b453e6319
Gustavo <honoratogustavo7@gmail.com>
2023-12-28 23:50:05 -0300

    atualizações

--- portifolio/src/components/BannerSection/BannerSection.jsx
+++ portifolio/src/components/BannerSection/BannerSection.jsx
@@ -1,6 +1,6 @@
 
 import styles from "./styles.module.css"
-import img from "../../assets/banner-img.png"
+import img from "../../assets/gustavo.png"
 
 export const BannerSection = () => {
     return (
@@ -8,7 +8,7 @@ export const BannerSection = () => {
             <div className={styles.BannerSectionTexts}>
                 <p className={styles.BannerTitle}>Gustavo Honorato</p>
                 <p className={styles.Title}>Bem vindo ao meu portfólio</p>
-                <p className={styles.Text}>Aspirante a desenvolvedor e bonito</p>
+                <p className={styles.Text}>Desenvolvedor Full Stack em formação</p>
                 <button className={styles.ButtonSectionBanner}>Saiba Mais</button> 
             </div>
             <img className={styles.ImgBanner} src={img} alt="Imagem computador" />
